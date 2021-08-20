3a55c2eb741afdcc7f7a3691b5739e4715c7cf32
pdd <drift1996@foxmail.com>
2021-08-20 19:10:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,21 +15,8 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo ' src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-
-
-
-
-
-
-#添加额外非必须软件包
-#网络测试
-git clone https://github.com/sirpdboy/NetSpeedTest package/lean/NetSpeedTest
-
-
-
 
 
 
