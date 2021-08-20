0e991d573b1cacfa0a3ae183bdeedf8dad8496f8
pdd <drift1996@foxmail.com>
2021-08-20 18:20:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,29 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
-# 第三方软件源
-git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
-
-#添加额外非必须软件包
-#网络测试
-#git clone https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-
-
-
-#第三方主题
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
-
-
-#OpenClash小猫咪
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-
-#应用过滤
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
-#helloworld
-git clone https://github.com/fw876/helloworld.git package/helloworld
-
-#多合一通知插件
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
-
 
